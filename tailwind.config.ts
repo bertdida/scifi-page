59b408ec64ebe10e26953f18007008b5f12074f9
Herbert Verdida <38489033+bertdida@users.noreply.github.com>
2023-11-20 21:32:06 +0800

    chore: initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      orange: "#ff632c",
+      white: "#ffffff",
+      blue: "#0083fe",
+    },
+    fontFamily: {
+      druk: ["Druk Wide Bold", "sans"],
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
